70fd3052263dd9eaef87a798525d96b7746d85a1
MandresyRandriamarosoa <151185727+MandresyRandriamarosoa@users.noreply.github.com>
2024-02-27 21:23:18 +0300

    fix historique rdv

--- src/app/services/RendezVous.service.ts
+++ src/app/services/RendezVous.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 
 export class RendezVousService {
     
-  private apiUrl = environment.baseApiURL+'/client/my-rdv/65d7dba927f6f694d9151e34'; //URL GET HISTORIQUE 
+  private apiUrl = environment.baseApiURL+'/client/my-rdv/65dde36d9b01b0c109183039'; //URL GET HISTORIQUE 
 
   constructor(private http: HttpClient) { }
 
