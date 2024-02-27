d5b88f182f496d395e6f0b33aaadac588a650528
MandresyRandriamarosoa <151185727+MandresyRandriamarosoa@users.noreply.github.com>
2024-02-27 19:54:17 +0300

    mise en page interface

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -15,9 +15,36 @@ export class AppMenuComponent implements OnInit {
     ngOnInit() {
         this.model = [
             {
-                label: 'Home',
+                label: 'Rendez-Vous',
                 items: [
-                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+                    { label: 'Prendre', icon: 'pi pi-fw pi-check-square', routerLink: ['/'] },
+                    { label: 'Liste', icon: 'pi pi-fw pi-list', routerLink: ['/user/c/my-rdv'] },
+                ]
+            },
+            {
+                label: 'Favoris',
+                items: [
+                    { label: 'Services', icon: 'pi pi-fw pi-share-alt', routerLink: ['/'] },
+                    { label: 'Employés', icon: 'pi pi-fw pi-user', routerLink: ['/'] },
+                ]
+            },
+            {
+                label: 'Mon compte',
+                items: [
+                    { label: 'Recharger', icon: 'pi pi-fw pi-dollar', routerLink: ['/user/c/my-account'] },
+                ]
+            },
+            {
+                label: 'Services',
+                items: [
+                    { label: 'Ajouter', icon: 'pi pi-fw pi-pencil', routerLink: ['/user/m/service/add'] },
+                    { label: 'Liste', icon: 'pi pi-fw pi-list', routerLink: ['/user/m/service'] },
+                ]
+            },
+            {
+                label: 'Dépenses',
+                items: [
+                    { label: 'Ajouter', icon: 'pi pi-fw pi-pencil', routerLink: ['/user/m/depense'] },
                 ]
             }
         ];
