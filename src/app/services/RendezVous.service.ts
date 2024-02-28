7ec34749341e3ce66501b68987dae03b0cfbd67c
MandresyRandriamarosoa <151185727+MandresyRandriamarosoa@users.noreply.github.com>
2024-02-28 23:23:25 +0300

    fix integration historique rdv

--- src/app/services/RendezVous.service.ts
+++ src/app/services/RendezVous.service.ts
@@ -3,14 +3,16 @@ import { RendezVous } from '../models/RendezVous';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Client } from '../models/client';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class RendezVousService {
-    
-  private apiUrl = environment.baseApiURL+'/client/my-rdv/65dde36d9b01b0c109183039'; //URL GET HISTORIQUE 
+ 
+  client: Client = JSON.parse(localStorage.getItem('client') || '{}');
+  private apiUrl = environment.baseApiURL+'/client/my-rdv/'+this.client._id; //URL GET HISTORIQUE 
 
   constructor(private http: HttpClient) { }
 
